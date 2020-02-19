559df113c63a0511bceee41c2eef176d9b921955
AsianaR <46110409+AsianaR@users.noreply.github.com>
2020-02-19 23:10:05 +0200

    Add files via upload

---
+++ DelegaciaVes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        delegate int OperationFir(int x);
+
+        static void Main(string[] args)
+        {
+            int value = 4;
+            
+            OperationFir delFir = Sqr; 
+            int result = delFir(value);
+            Console.WriteLine(x2(result));
+            
+
+            Console.Read();
+        }
+        private static int Sqr(int x)
+        {
+            return x + x;
+        }
+        private static int x2(int delFir)
+        {
+            return (delFir * 2);
+        }
+    }
+}
